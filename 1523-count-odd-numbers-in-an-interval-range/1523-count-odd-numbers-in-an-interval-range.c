aafe82211f98dcdde2e302b600de96cd549b5ae0
Sriram kanakam <92006184+Sri-k23@users.noreply.github.com>
2022-06-29 11:42:02 +0400

    Time: 3 ms (50.00%), Space: 5.4 MB (62.47%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.c
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.c
@@ -1,10 +1,16 @@
 
 
 int countOdds(int low, int high){
-        if(low%2||high%2){
-        return (high-low)/2+1;
-    }
-    else{
-        return (high-low)/2;
-    }
+  if(low%2==0 && high%2==0)
+      return (high-low)/2;
+  else if(low%2!=0 && high%2==0)
+      return ((high-low)+1)/2;
+  else if(low%2==0 && high%2!=0)
+      return ((high-low)+1)/2;
+  else if(low%2!=0 && high%2!=0)
+      return (high-low)/2+1;
+  else if((high-low)==1)
+      return 1;
+  else
+      return 0;
 }
