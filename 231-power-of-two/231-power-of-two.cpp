6101ac2d6256b6145e0adede0af4254b1f217f8c
Sriram kanakam <92006184+Sri-k23@users.noreply.github.com>
2022-08-01 14:56:37 +0530

    Time: 4 ms (41.35%), Space: 5.9 MB (26.52%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -3,7 +3,8 @@ public:
     bool isPowerOfTwo(int n) {
         int i=0;
         while(i<=30){
-            if(n == pow(2,i)){
+            int ans = pow(2,i);
+            if(n == ans){
                 return true;
             }
           i++;
